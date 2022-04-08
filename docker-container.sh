f4b539aef35ee5ac74a526f0b115266be8c7e686
fahmiaz411 <fahmiaziz884@gmail.com>
2022-04-08 13:51:18 +0700

    container

---
+++ docker-container.sh
@@ -0,0 +1,13 @@
+docker container ls
+
+docker container ls -a
+
+docker container create --name contohredis redis:latest
+
+docker container start contohredis
+
+docker container stop contohredis
+
+docker container logs contohredis
+
+docker container logs -f contohredis
